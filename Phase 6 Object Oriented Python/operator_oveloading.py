da6635a095ab40980b36758551e832221952d0a3
sahillodha <sahil.lodha@deerwalk.edu.np>
2021-01-26 00:24:25 +0545

    Added to operator_oveloading.py.

--- Phase 6 Object Oriented Python/operator_oveloading.py
+++ Phase 6 Object Oriented Python/operator_oveloading.py
@@ -14,6 +14,17 @@ class Vector:
         else:
             return f'The Details of the vector are:\nX - Coordinate: {self.__x_coordinate}\nY - Coordinate: {self.__y_coordinate}\nZ - Coordinate: {self.__z_coordinate}\n'
 
+    def __mul__(self, other):
+        x_prod = self.__x_coordinate * other.__x_coordinate
+        y_prod = self.__y_coordinate * other.__y_coordinate
+        z_prod = self.__z_coordinate * other.__z_coordinate
+        return x_prod + y_prod + z_prod
+
+    def __add__(self, other):
+        angle = (self * other)/(self.magnitude() * other.magnitude())
+        value = self.magnitude() ** 2 + other.magnitude() ** 2 + 2*self.magnitude()*other.magnitude()*angle
+        return value**(1/2)
+
 
 pointObj = Vector(1, 2, 2)
 print(pointObj.magnitude())
