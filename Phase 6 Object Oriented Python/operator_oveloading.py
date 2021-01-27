aafb6dd133c1cc4f5a5db31f11dd9668c8803c25
sahillodha <sahil.lodha@deerwalk.edu.np>
2021-01-27 23:30:23 +0545

    Added Web Scraping module.

--- Phase 6 Object Oriented Python/operator_oveloading.py
+++ Phase 6 Object Oriented Python/operator_oveloading.py
@@ -4,6 +4,27 @@ class Vector:
         self.__y_coordinate = y_coordinate
         self.__z_coordinate = z_coordinate
 
+    # The property decorator creates a property vector for the class Vector
+    @property
+    def vector(self):
+        return f'{self.__x_coordinate}i + {self.__y_coordinate}j + {self.__z_coordinate}k'
+
+    # Once a property has been created then the setter can be created ..
+    @vector.setter
+    def vector(self, value):
+        value = value.split(' ')
+        self.__x_coordinate = value[0]
+        self.__y_coordinate = value[1]
+        self.__z_coordinate = value[2]
+
+    # This is a destructor function which is called during the destruction of the vector property.
+    @vector.deleter
+    def vector(self):
+        print("Deleting Vector!")
+        self.__x_coordinate = None
+        self.__y_coordinate = None
+        self.__z_coordinate = None
+
     def magnitude(self):
         mag = (self.__x_coordinate**2 + self.__y_coordinate**2 + self.__z_coordinate**2)**(1/2)
         return mag
@@ -28,5 +49,14 @@ class Vector:
         return value**(1/2)
 
 
-pointObj = Vector(1, 2, 2)
-print(pointObj.magnitude())
+vector1 = Vector(1, 2, 2)
+vector2 = Vector(1, 2, 2)
+print(vector1+vector2)
+print(vector1*vector2)
+print(vector1.magnitude())
+print(vector2.magnitude())
+print("<<<<< @property decorator : Result >>>>>>")
+print(vector1.vector)
+vector1.vector = '1 2 4'
+print(vector1)
+del vector1.vector
